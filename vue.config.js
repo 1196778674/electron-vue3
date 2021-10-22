0e702693792462e4c9593eafd951fc5a51987aac
刘飞 <liufei@bizseer.com>
2021-10-22 14:55:48 +0800

    win 64

--- vue.config.js
+++ vue.config.js
@@ -42,7 +42,7 @@ module.exports = {
                         "target": "nsis", //利用nsis制作安装程序
                         "arch": [
                             "x64", //64位
-                            "ia32" //32位
+                            // "ia32" //32位
                         ]
                     }]
                 },
